d11ba7f3984c4842e50567131a56c68c850dc833
Ibragimov Igor <ibragimovia@yandex.ru>
2023-03-27 13:02:09 +0500

    Добавлен алгоритм чтения данных с логического диска

--- FAT32.cpp
+++ FAT32.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    const WCHAR *fileName = L"\\\\.\\G:";
+    const WCHAR *fileName = L"\\\\.\\E:";
 	HANDLE fileHandle = CreateFileW(
 		fileName, // Имя файла (WCHAR*)
 		GENERIC_READ,	  // Режим доступа
@@ -19,7 +19,36 @@ int main()
 		cout << "Ошибка чтения диска" << endl;
 		return 1;
 	}
+	BYTE dataBuffer[512];
+	ULONGLONG startOffset = 0x0;
+	DWORD bytesToRead = 512;
+	DWORD bytesRead;
+	LARGE_INTEGER sectorOffset;
+	sectorOffset.QuadPart = startOffset;
+	unsigned long currentPosition = SetFilePointer(
+		fileHandle,
+		sectorOffset.LowPart,
+		&sectorOffset.HighPart,
+		FILE_BEGIN // Точка в файле, относительно которой необходимо позиционироваться (FILE_BEGIN, FILE_CURRENT, FILE_END)
+	);
 
+	if (currentPosition != sectorOffset.LowPart)
+	{
+		cout << "Курсор не установлен в начале" << endl;
+		return 2;
+	}
+	bool readResult = ReadFile(
+		fileHandle,
+		dataBuffer,
+		bytesToRead,
+		&bytesRead,
+		NULL
+	);
+	if (!readResult || bytesRead != bytesToRead)
+	{
+		cout << "Чтение данных выполнено с ошибкой" << endl;
+		return 3;
+	}
 	CloseHandle(fileHandle);
 	return 0;
 }
