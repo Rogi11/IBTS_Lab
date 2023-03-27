02c4df922359d8539bfae80ba4560809cc8ae68f
Ibragimov Igor <ibragimovia@yandex.ru>
2023-03-27 12:30:46 +0500

    Написание кода для чтения логического диска

--- FAT32.cpp
+++ FAT32.cpp
@@ -1,6 +1,25 @@
 ﻿#include <iostream>
-
+#include <Windows.h>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+    const WCHAR *fileName = L"\\\\.\\G:";
+	HANDLE fileHandle = CreateFileW(
+		fileName, // Имя файла (WCHAR*)
+		GENERIC_READ,	  // Режим доступа
+		FILE_SHARE_READ | FILE_SHARE_WRITE, // Режим совместной работы
+		NULL, // Атрибуты безопасности
+		OPEN_EXISTING, // Способ открытия
+		FILE_ATTRIBUTE_NORMAL, // Флаги и атрибуты
+		NULL // Описатель (идентификатор) файла шаблона с правами доступа GENERIC_READ.
+	);
+	if (fileHandle == INVALID_HANDLE_VALUE)
+	{
+		cout << "Ошибка чтения диска" << endl;
+		return 1;
+	}
+
+	CloseHandle(fileHandle);
+	return 0;
 }
